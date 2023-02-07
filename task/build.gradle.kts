e0e92631e046bca094a3b2e202da82b96687775e
Nikita Koshelev <hukuta94@users.noreply.github.com>
2023-02-08 00:08:13 +0500

    use test fixtures instead of mocks

--- task/build.gradle.kts
+++ task/build.gradle.kts
@@ -33,7 +33,6 @@ dependencies {
     // tests
     testCompileOnly("org.junit.jupiter:junit-jupiter-api:5.9.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     testImplementation("io.arrow-kt:arrow-core:1.1.5")
     testImplementation("io.kotest:kotest-runner-junit5:5.0.0")
     testImplementation("io.kotest.extensions:kotest-assertions-arrow-jvm:1.1.1")
--- task/src/test/kotlin/com/stringconcat/todo/cleanarchitecture/task/TaskFactoryTest.kt
+++ task/src/test/kotlin/com/stringconcat/todo/cleanarchitecture/task/TaskFactoryTest.kt
@@ -2,25 +2,18 @@ package com.stringconcat.todo.cleanarchitecture.task
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.any
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
-import java.time.LocalDateTime
 
 class TaskFactoryTest {
     @Test
     fun `correct input data - must create task and change priority`() {
         // given
-        val priorityProvider: PriorityProvider = mock()
-        val taskFactory = TaskFactory(priorityProvider)
+        val taskFactory = taskFactory(middlePriorityProvider())
 
-        val expectedTaskDescription = requireNotNull(TaskDescription.of("task description").getOrNull())
-        val expectedTaskDeadline = requireNotNull(TaskDeadline.of(LocalDateTime.now().plusDays(1L)).getOrNull())
+        val expectedTaskDescription = taskDescription()
+        val expectedTaskDeadline = taskDeadline()
         val expectedPriority = Task.Priority.MIDDLE
 
         // when
-        whenever(priorityProvider.calculatePriority(any())).thenReturn(expectedPriority)
-
         val actualTask = taskFactory.createTask(
             expectedTaskDescription,
             expectedTaskDeadline
