30d780eb3c18ccfb3900674d5f5053c23ab062c1
Nikita Koshelev <hukuta94@users.noreply.github.com>
2023-02-19 21:35:25 +0500

    add tests for business logic of neural network

--- neural-network/src/test/kotlin/com/stringconcat/todo/cleanarchitecture/neuralnetwork/NeuralNetworkTaskPriorityProviderTest.kt
+++ neural-network/src/test/kotlin/com/stringconcat/todo/cleanarchitecture/neuralnetwork/NeuralNetworkTaskPriorityProviderTest.kt
@@ -1,8 +1,18 @@
 package com.stringconcat.todo.cleanarchitecture.neuralnetwork
 
 import com.stringconcat.todo.cleanarchitecture.domain.task.Task
+import com.stringconcat.todo.cleanarchitecture.domain.task.TaskDeadline
+import com.stringconcat.todo.cleanarchitecture.domain.task.TaskDescription
+import com.stringconcat.todo.cleanarchitecture.domain.testfixtures.task
+import com.stringconcat.todo.cleanarchitecture.domain.testfixtures.taskDeadline
+import com.stringconcat.todo.cleanarchitecture.domain.testfixtures.taskDescription
 import io.kotest.matchers.longs.shouldBeExactly
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.util.*
 
 class NeuralNetworkTaskPriorityProviderTest {
@@ -39,6 +49,55 @@ class NeuralNetworkTaskPriorityProviderTest {
         }
     }
 
+    @Test
+    fun `neuralNetworkValueToPriority - unsupported negative value - should throw exception`() {
+        executePropertyBasedTest {
+            val number = (Long.MIN_VALUE..-1).random()
+            assertThrows<IllegalStateException> {
+                neuralNetworkTaskPriorityProvider.neuralNetworkValueToPriority(number)
+            }
+        }
+    }
+
+    @Test
+    fun `neuralNetworkValueToPriority - unsupported positive value - should throw exception`() {
+        executePropertyBasedTest {
+            val number = (3..Long.MAX_VALUE).random()
+            assertThrows<IllegalStateException> {
+                neuralNetworkTaskPriorityProvider.neuralNetworkValueToPriority(number)
+            }
+        }
+    }
+
+    @Test
+    fun `neuralNetworkValueToPriority - value 0 - should be low priority`() {
+        neuralNetworkTaskPriorityProvider.neuralNetworkValueToPriority(0) shouldBe Task.Priority.LOW
+    }
+
+    @Test
+    fun `neuralNetworkValueToPriority - value 1 - should be middle priority`() {
+        neuralNetworkTaskPriorityProvider.neuralNetworkValueToPriority(1) shouldBe Task.Priority.MIDDLE
+    }
+
+    @Test
+    fun `neuralNetworkValueToPriority - value 2 - should be high priority`() {
+        neuralNetworkTaskPriorityProvider.neuralNetworkValueToPriority(2) shouldBe Task.Priority.HIGH
+    }
+
+    @Test
+    fun `calculatePriority - property test - should calculate priority`() {
+        executePropertyBasedTest {
+            val descriptionText = "a".repeat((0..10000).random())
+            val deadLineEpochSecond =
+                (LocalDateTime.now().toEpochSecond(ZoneOffset.UTC)..LocalDateTime.MAX.toEpochSecond(ZoneOffset.UTC)).random()
+
+            executeExampleBasedTest(
+                taskDescription(descriptionText),
+                taskDeadline(deadLineEpochSecond)
+            )
+        }
+    }
+
     /**
      * Executes Property-Based Test that should be repeated a lot of times
      * @param function lambda-expression of test
@@ -47,6 +106,31 @@ class NeuralNetworkTaskPriorityProviderTest {
         for (i in 0..PROPERTY_BASED_TEST_REPEAT_COUNT) {
             function()
         }
+    }
+
+    /**
+     * Executes once Example-Based Test by open-box principle with specified params
+     * @param taskDescription description of domain object [Task]
+     * @param taskDeadline deadline of domain object [Task]
+     */
+    private fun executeExampleBasedTest(
+        taskDescription: TaskDescription,
+        taskDeadline: TaskDeadline
+    ) {
+        // given
+        val task = task(taskDescription, taskDeadline)
+        val sum = neuralNetworkTaskPriorityProvider.sum(
+            task.description.toString().length.toLong(),
+            task.deadline.toLocalDateTime().toEpochSecond(ZoneOffset.UTC)
+        )
+        val mod = neuralNetworkTaskPriorityProvider.mod(sum, TASK_PRIORITY_COUNT_OF_VALUES)
+        val expectedPriority = neuralNetworkTaskPriorityProvider.neuralNetworkValueToPriority(mod)
+
+        // when
+        val actualPriority = neuralNetworkTaskPriorityProvider.calculatePriority(task)
+
+        // then
+        assertEquals(expectedPriority, actualPriority)
     }
 
     companion object {
